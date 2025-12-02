2645dbc9dac8f424b10ec6259824a0cc4d3d15b9
karthik nadar <karthiknadar1204@gmail.com>
2025-12-02 20:51:52 +0530

    stupid

--- hello-fastapi/src/todos/config/db.py
+++ hello-fastapi/src/todos/config/db.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine
 import os
 
-connection_string = os.getenv('DB_URI') or 'postgresql://karthiknadar1204:Fvph9DyfVm2L@ep-restless-credit-a1c7489o-pooler.ap-southeast-1.aws.neon.tech/dvfdsbbg?sslmode=require&channel_binding=require'
+connection_string = os.getenv('DB_URI')
 # postgresql://karthiknadar1204:Fvph9DyfVm2L@ep-restless-credit-a1c7489o-pooler.ap-southeast-1.aws.neon.tech/dvfdsbbg?sslmode=require&channel_binding=require
 print(connection_string)
 engine = create_engine(connection_string)
