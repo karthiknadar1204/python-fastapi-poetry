e1f68f1972661d676d1cee0f0dafe966ac0416cc
karthik nadar <karthiknadar1204@gmail.com>
2025-12-02 20:51:08 +0530

    todo basics fastapi

---
+++ hello-fastapi/src/todos/config/db.py
@@ -0,0 +1,10 @@
+from sqlmodel import SQLModel, create_engine
+import os
+
+connection_string = os.getenv('DB_URI') or 'postgresql://karthiknadar1204:Fvph9DyfVm2L@ep-restless-credit-a1c7489o-pooler.ap-southeast-1.aws.neon.tech/dvfdsbbg?sslmode=require&channel_binding=require'
+# postgresql://karthiknadar1204:Fvph9DyfVm2L@ep-restless-credit-a1c7489o-pooler.ap-southeast-1.aws.neon.tech/dvfdsbbg?sslmode=require&channel_binding=require
+print(connection_string)
+engine = create_engine(connection_string)
+
+def create_tables():
+    SQLModel.metadata.create_all(engine)
